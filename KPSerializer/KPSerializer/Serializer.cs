4c6cf50437afd23566c8350b2d3bbe8d92e55c19
Halit YURTTAŞ <halityurttas@gmail.com>
2015-12-04 11:42:36 +0200

    Addition serializer class

---
+++ KPSerializer/KPSerializer/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace KPSerializer
+{
+    public static class Serializer
+    {
+        public static string Serialize(List<KPModel> model)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<KPModel>));
+            StringBuilder tempSB = new StringBuilder();
+            StringWriter stringStream = new StringWriter(tempSB);
+            serializer.Serialize(stringStream, model);
+            return tempSB.ToString();
+        }
+
+        public static List<KPModel> Deserialize(string data)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<KPModel>));
+            StringReader stringStrem = new StringReader(data);
+            return serializer.Deserialize(stringStrem) as List<KPModel>;
+        }
+    }
+}
