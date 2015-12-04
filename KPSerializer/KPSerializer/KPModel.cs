4da4688ad1a3e2b80dcebf817af42885e276356c
Halit YURTTAŞ <halityurttas@gmail.com>
2015-12-04 11:42:04 +0200

    Project Complete #1

---
+++ KPSerializer/KPSerializer/KPModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace KPSerializer
+{
+    [Serializable]
+    public class KPModel
+    {
+        [XmlElement]
+        public string Key { get; set; }
+        [XmlElement]
+        public string Value { get; set; }
+    }
+}
